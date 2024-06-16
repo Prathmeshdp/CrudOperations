55bd3705d99013df0e58e9fc1d47a8ddb4c2e306
Prathmeshdp <prathmeshdeelippatil@gmail.com>
2024-06-16 15:50:22 +0530

    Solve service layer isuue

--- src/main/java/com/BikkadIT/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/BikkadIT/demo/service/impl/UserServiceImpl.java
@@ -4,10 +4,12 @@ import com.BikkadIT.demo.model.User;
 import com.BikkadIT.demo.repository.UserRepository;
 import com.BikkadIT.demo.service.UserServiceI;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserServiceImpl implements UserServiceI {
 
     @Autowired
